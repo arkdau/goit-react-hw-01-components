81862cf784497df4130e8c224183adc1246c78fe
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-11-13 11:18:18 +0100

    feat: add PropTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import './Profile.css';
+import { PropTypes } from 'prop-types';
 
 const Profile = ({username, tag, location, avatar, stats}) => (
   <div className='profile'>
@@ -29,5 +30,17 @@ const Profile = ({username, tag, location, avatar, stats}) => (
     </ul>
   </div>
 )
+
+Profile.propTypes ={
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired
+  })
+}
 
 export default Profile
