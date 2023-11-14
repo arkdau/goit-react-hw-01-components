77f7ecc85289035b7e36bdf5b7632dc473920ee3
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-11-14 01:55:57 +0100

    feat: add clsx module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
 import user from './Profile/user.json';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
@@ -12,17 +14,8 @@ export const App = () => {
         stats={user.stats}
       />
 
+     <Statistics title="Upload stats" stats={data} />
+     <Statistics stats={data} />
     </>
-
-
-
-
-
-
-
-
-
-
-
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import clsx from 'clsx';
+ import css from './Statistics.module.css';
+ // import { element, PropTypes } from 'prop-types';
+
+
+const Statistics = ({title, stats}) => (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+
+      <ul className={css.statList}>
+       {stats.map((item, index) => {
+          let color = [css.color0, css.color1, css.color2, css.color3];
+          return (
+               <li key={item.id} className={clsx(css.item, color[index])}>
+                   <span className={css.label}>{item.label}</span>
+                   <span className={css.percentage}>{item.percentage}</span>
+               </li>
+      )
+       })}
+     </ul>
+    </section>
+)
+
+export default Statistics
+
